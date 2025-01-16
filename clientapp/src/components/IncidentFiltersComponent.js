5d34c9f8f866d3c3190a19960af1d1c5c86b7ee6
Марченко Сергей Владимирович <smarchenko@ussc.ru>
2025-01-16 14:45:43 +0500

    редактирование сохраненных фильтров 3.5.5  2025-01-16 14:45:43

--- clientapp/src/components/IncidentFiltersComponent.js
+++ clientapp/src/components/IncidentFiltersComponent.js
@@ -571,20 +571,7 @@ const handleBackButtonClick = (e) => {
 
       <div className="tooltip-container">
   <button className="add-filter-button" onClick={handleAddFilterClick}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1"
-      height="1"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-filter"
-    >
-      <polygon points="22 3 2 3 10 12.5 10 19 14 21 14 12.5 22 3"></polygon>
-    </svg>
+    
     <span>{getAddFilterButtonText()}</span>
     {activeFilter && (
       <Tooltipp text="Перевернуть стрелку">
